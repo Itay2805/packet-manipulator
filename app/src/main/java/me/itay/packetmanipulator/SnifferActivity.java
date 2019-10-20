f86a82559d106a70c73e705d4cb70ff8a920fa16
itay2805 <itay2805@protonmail.com>
2019-10-20 15:23:18 +0300

    Added navigation menu
    Now we can stop the auto scroll, goto first and last
    
    soon I will implement the stop and save functionality :)

--- app/src/main/java/me/itay/packetmanipulator/SnifferActivity.java
+++ app/src/main/java/me/itay/packetmanipulator/SnifferActivity.java
@@ -1,6 +1,12 @@
 package me.itay.packetmanipulator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import me.itay.packetmanipulator.display.PacketEntry;
@@ -8,8 +14,14 @@ import me.itay.packetmanipulator.display.PacketDissector;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.pcap4j.core.NotOpenException;
 import org.pcap4j.core.PcapHandle;
@@ -20,15 +32,22 @@ import org.pcap4j.packet.Packet;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class SnifferActivity extends AppCompatActivity implements Runnable {
+public class SnifferActivity extends AppCompatActivity implements Runnable, NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "SnifferActivity";
 
     private PcapNetworkInterface pif;
     private PcapHandle handle;
+    private boolean autoScroll = true;
 
     private Thread captureThread;
     private ConcurrentLinkedQueue<Packet> packets = new ConcurrentLinkedQueue<>();
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView optionsView;
+    private TextView lblInterfaceName;
+    private TextView lblInterfaceDescription;
 
     private Button btnSetFilter;
     private EditText txtFilterExpression;
@@ -40,16 +59,32 @@ public class SnifferActivity extends AppCompatActivity implements Runnable {
         super.onCreate(savedInstanceState);
 
         // set layout and get controls
-        setContentView(R.layout.activity_sniffer);
+        setContentView(R.layout.sniffer_activity);
+
+        /////////////////////////////////////////////////////////////////
+        // get the controls
+        /////////////////////////////////////////////////////////////////
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.sniffer_drawer);
+        optionsView = findViewById(R.id.options_view);
+        setSupportActionBar(toolbar);
+
+        View headerView = optionsView.getHeaderView(0);
+        lblInterfaceName = headerView.findViewById(R.id.lblInterfaceName);
+        lblInterfaceDescription = headerView.findViewById(R.id.lblInterfaceDescription);
+        optionsView.setNavigationItemSelectedListener(this);
+        optionsView.setCheckedItem(R.id.option_auto_scroll);
+
         btnSetFilter = findViewById(R.id.btnSetFilter);
         txtFilterExpression = findViewById(R.id.txtFilterExpression);
         recyclerPackets = findViewById(R.id.recyclerPackets);
         adapter = new RecyclerPacketsAdapter();
-
         recyclerPackets.setAdapter(adapter);
         recyclerPackets.setLayoutManager(new LinearLayoutManager(this));
 
+        /////////////////////////////////////////////////////////////////
         // get the device
+        /////////////////////////////////////////////////////////////////
         String pifName = getIntent().getStringExtra("INTERFACE_NAME");
         pif = Pcaps.getDevByName(pifName);
         assert pif != null;
@@ -64,6 +99,17 @@ public class SnifferActivity extends AppCompatActivity implements Runnable {
         captureThread = new Thread(this, "SnifferActivityCapture");
         captureThread.start();
 
+        /////////////////////////////////////////////////////////////////
+        // finish setting the layout
+        /////////////////////////////////////////////////////////////////
+
+        // set the interface info
+        lblInterfaceName.setText(String.format("%s (%s)", pif.getName(), handle.getDlt().name()));
+        if(pif.getDescription() != null) {
+            lblInterfaceDescription.setText(pif.getDescription());
+        }else {
+            lblInterfaceDescription.setText("No description");
+        }
     }
 
     @Override
@@ -88,13 +134,18 @@ public class SnifferActivity extends AppCompatActivity implements Runnable {
                 adapter.add(entry);
                 recyclerPackets.post(() -> {
                     adapter.notifyDataSetChanged();
-                    recyclerPackets.scrollToPosition(adapter.getItemCount());
+
+                    if(autoScroll) {
+                        recyclerPackets.scrollToPosition(adapter.getItemCount() - 1);
+                    }
                 });
             } catch (NotOpenException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
 
     @Override
     protected void onDestroy() {
@@ -104,6 +155,44 @@ public class SnifferActivity extends AppCompatActivity implements Runnable {
             handle.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+
+            case R.id.option_save:
+
+                break;
+
+            case R.id.option_auto_scroll:
+                autoScroll = !autoScroll;
+                menuItem.setChecked(autoScroll);
+                break;
+
+            case R.id.option_go_to_first:
+                recyclerPackets.scrollToPosition(0);
+                break;
+
+            case R.id.option_go_to_last:
+                recyclerPackets.scrollToPosition(adapter.getItemCount() - 1);
+                break;
+
+            default:
+                return false;
+        }
+
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        }else {
+            super.onBackPressed();
         }
     }
 }
